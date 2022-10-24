73973f1c0506d72f9930acbb4519626b0efae302
Adam Shea <avidworks@live.com>
2022-10-23 23:48:53 -0400

    fix checkbox

--- components/forms/checkbox.jsx
+++ components/forms/checkbox.jsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 const Checkbox = forwardRef(({ name, label, ...rest }, ref) => {
 	return (
 		<label className="mt-1 mb-1 flex items-center">
-			<input type="checkbox" name={name} {...rest} ref={ref} className="w-full" />
+			<input type="checkbox" name={name} {...rest} ref={ref} />
 			<span className="ml-3 block">{label}</span>
 		</label>
 	);
